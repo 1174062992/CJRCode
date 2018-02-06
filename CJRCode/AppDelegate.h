a0b8d2a40073e7f2e55a4969f371e1f0ab42121a
1174062992 <1174062992>
2018-02-06 09:55:37 +0800

    Initial Commit

---
+++ CJRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJRCode
+//
+//  Created by 曹记 on 2018/2/6.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
